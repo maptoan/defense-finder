2781e41f64e2d7f819c1d1025af5f8a9699dcf80
toanmap <toanmap@gmail.com>
2026-03-30 12:52:53 +0700

    fix(security): path traversal in file export
    The `export_defense_finder_genes` function joins `outdir` and `filename` using `os.path.join` without validating that the resulting path remains within the intended directory. If `filename` contains path traversal sequences (e.g., '../../etc/passwd'), an attacker could write files to arbitrary locations on the filesystem.
    
    Affected files: df_genes.py, df_hmmer.py
    
    Signed-off-by: toanmap <174589430+maptoan@users.noreply.github.com>

--- defense_finder_posttreat/df_genes.py
+++ defense_finder_posttreat/df_genes.py
@@ -2,4 +2,5 @@ import os
 
 
 def export_defense_finder_genes(defense_finder_genes, outdir, filename):
-    defense_finder_genes.to_csv(os.path.join(outdir, filename+'_defense_finder_genes.tsv'), sep='\t', index=False)
+    safe_filename = os.path.basename(filename) + '_defense_finder_genes.tsv'
+    defense_finder_genes.to_csv(os.path.join(outdir, safe_filename), sep='	', index=False)
